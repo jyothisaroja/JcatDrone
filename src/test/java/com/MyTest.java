af99dd77c63bffad6f7cb25050954244a4142c37
root <root@instance-1.asia-south1-c.c.even-metrics-234003.internal>
2019-03-12 04:41:15 +0000

    First commit

---
+++ src/test/java/com/MyTest.java
@@ -0,0 +1,11 @@
+package com;
+import org.testng.annotations.Test;
+public class MyTest{
+
+@Test
+public void myTest(){
+System.out.println("**********************Hello!!!!!Welocme to java from drone***********************");
+}
+
+}
+
